da3400a764d9f922db0c1537d4f78331264c025a
Roman Sydoruk <sydoruk89@gmail.com>
2020-10-16 00:12:27 -0700

    model

--- stocks/models.py
+++ stocks/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Stock(models.Model):
     name = models.CharField(max_length=64)
+    price = models.CharField(max_length=64, null=True)
 
     def __str__(self):
         return self.name
